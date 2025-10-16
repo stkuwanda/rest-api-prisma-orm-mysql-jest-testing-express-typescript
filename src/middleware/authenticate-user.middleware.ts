8b03ef1aef1f98afffc85dbb7ceaaaa79611cb29
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-10-16 16:37:37 +0200

    use auth0 generated token

--- src/middleware/authenticate-user.middleware.ts
+++ src/middleware/authenticate-user.middleware.ts
@@ -4,7 +4,6 @@ import config from '../config';
 const authenticateUser = auth({
   audience: config.audience,
   issuerBaseURL: config.issuerBaseUrl,
-  tokenSigningAlg: 'RS256',
 });
 
 export default authenticateUser;
